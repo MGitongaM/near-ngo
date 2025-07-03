3138428654aed94aedfda47ce722fb5e63ea8713
Michael Gitonga <gitoshmbae@gmail.com>
2025-07-03 11:54:54 +0300

    Adjust the classes to make the navigation responsive

--- src/components/general/Navigation.tsx
+++ src/components/general/Navigation.tsx
@@ -20,60 +20,62 @@ import { MenuIcon } from "lucide-react";
 
 export default function Navigation() {
   return (
-    <nav className="w-full md:container mx-auto  md:px-4 ">
-      <NavigationMenu className="">
-        <NavigationMenuList className="flex justify-between items-center w-full lg:w-[90vw] mx-auto min-h-[10dvh] fixed bg-white blur-md z-10 ">
-          <NavigationMenuItem>
-            <Link href="/">
-            <Image
-              src="/images/NEAR logo.png"
-              width={800}
-              height={800}
-              alt="Near Ngo Home Hero Image"
-              className="object-cover w-28 p-2 mx-auto"
-            />
-            </Link>
-          </NavigationMenuItem>
-          <div className="hidden md:flex gap px-10 justify-">
-            {navigationEntries.map((entry) => (
-              <NavigationMenuItem key={entry.id}>
-                <NavigationMenuLink
-                  asChild
-                  className={navigationMenuTriggerStyle()}
-                >
-                  <Link href={entry.link}>{entry.name}</Link>
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-            ))}
-          </div>
-          {/* Mobile Navigation  */}
-          <div className="block lg:hidden">
-            <Sheet>
-              <SheetTrigger>
-                <MenuIcon className="size-10" />
-              </SheetTrigger>
-              <SheetContent side='top' className="">
-                <SheetHeader>
-                  <SheetTitle></SheetTitle>
-                  <SheetDescription></SheetDescription>
-                </SheetHeader>
-                <div className="flex flex-col px-8  gap-2 justify-end list-none">
-                  {navigationEntries.map((entry) => (
-                    <NavigationMenuItem key={entry.id}>
-                      <NavigationMenuLink
-                        asChild
-                        className={navigationMenuTriggerStyle()}
-                      >
-                        <Link href={entry.link}>{entry.name}</Link>
-                      </NavigationMenuLink>
-                    </NavigationMenuItem>
-                  ))}
-                </div>
-              </SheetContent>
-            </Sheet>
-          </div>
-        </NavigationMenuList>
-      </NavigationMenu>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-white blur-md">
+      <div className="container mx-auto px-4">
+        <NavigationMenu className="">
+          <NavigationMenuList className="flex justify-between items-center w-screen md:px-20 mx-auto min-h-[10dvh] ">
+            <NavigationMenuItem className="size-28">
+              <Link href="/">
+                <Image
+                  src="/images/NEAR logo.png"
+                  width={800}
+                  height={800}
+                  alt="Near Ngo Home Hero Image"
+                  className="object-contain size-28 p-2 mx-auto"
+                />
+              </Link>
+            </NavigationMenuItem>
+            <div className="hidden md:flex gap px-10 justify-">
+              {navigationEntries.map((entry) => (
+                <NavigationMenuItem key={entry.id}>
+                  <NavigationMenuLink
+                    asChild
+                    className={navigationMenuTriggerStyle()}
+                  >
+                    <Link href={entry.link}>{entry.name}</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
+            </div>
+            {/* Mobile Navigation  */}
+            <div className="block md:hidden">
+              <Sheet>
+                <SheetTrigger>
+                  <MenuIcon className="size-10" />
+                </SheetTrigger>
+                <SheetContent side="top" className="">
+                  <SheetHeader>
+                    <SheetTitle></SheetTitle>
+                    <SheetDescription></SheetDescription>
+                  </SheetHeader>
+                  <div className="flex flex-col px-8  gap-2 justify-end list-none">
+                    {navigationEntries.map((entry) => (
+                      <NavigationMenuItem key={entry.id}>
+                        <NavigationMenuLink
+                          asChild
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          <Link href={entry.link}>{entry.name}</Link>
+                        </NavigationMenuLink>
+                      </NavigationMenuItem>
+                    ))}
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
     </nav>
   );
 }
